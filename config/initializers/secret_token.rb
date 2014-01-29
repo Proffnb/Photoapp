206c384462a07ab72b83e44c6ce11425733c0067
Nate Bankirer <natebankirer@gmail.com>
2014-01-28 16:12:58 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoapp::Application.config.secret_key_base = '58e6dca79f31fef3ddd606f30262c774bd5e6eb12e87653351a0f3db1d93415d220dd369da8935133e405df00b026f465abd399c0ddee95a94fc7875f2a1ab33'
